0150739c6b82f40888b61015eaccd24ff1985a7c
Zachary Anderson <zra@google.com>
2016-05-23 12:11:39 -0700

    Dart: Roll Dart runtime
    Review URL: https://codereview.chromium.org/2004943002 .

--- tools/dart/update.py
+++ tools/dart/update.py
@@ -18,7 +18,7 @@ import sys
 # How to roll the dart sdk: Just change this url! We write this to the stamp
 # file after we download, and then check the stamp file for differences.
 SDK_URL_BASE = ('https://gsdview.appspot.com/dart-archive/channels/dev/raw/'
-                '1.17.0-dev.2.0/sdk/')
+                '1.17.0-dev.4.1/sdk/')
 
 LINUX_64_SDK = 'dartsdk-linux-x64-release.zip'
 MACOS_64_SDK = 'dartsdk-macos-x64-release.zip'
