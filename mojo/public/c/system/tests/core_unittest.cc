99726297278840957f92c2e4d3e59724a87cc437
Viet-Trung Luu <viettrungluu@chromium.org>
2016-06-01 10:39:39 -0700

    Start splitting core_unittest.cc up.
    R=azani@chromium.org
    
    Review URL: https://codereview.chromium.org/2028153002 .

--- mojo/public/c/system/tests/core_unittest.cc
+++ mojo/public/c/system/tests/core_unittest.cc
@@ -21,13 +21,8 @@ extern "C" const char* MinimalCTest(void);
 // Defined in core_unittest_pure_cpp.cc.
 const char* MinimalCppTest();
 
-namespace mojo {
 namespace {
 
-const MojoHandleRights kDefaultMessagePipeHandleRights =
-    MOJO_HANDLE_RIGHT_TRANSFER | MOJO_HANDLE_RIGHT_READ |
-    MOJO_HANDLE_RIGHT_WRITE | MOJO_HANDLE_RIGHT_GET_OPTIONS |
-    MOJO_HANDLE_RIGHT_SET_OPTIONS;
 const MojoHandleRights kDefaultDataPipeProducerHandleRights =
     MOJO_HANDLE_RIGHT_TRANSFER | MOJO_HANDLE_RIGHT_WRITE |
     MOJO_HANDLE_RIGHT_GET_OPTIONS | MOJO_HANDLE_RIGHT_SET_OPTIONS;
@@ -39,12 +34,6 @@ const MojoHandleRights kDefaultSharedBufferHandleRights =
     MOJO_HANDLE_RIGHT_GET_OPTIONS | MOJO_HANDLE_RIGHT_SET_OPTIONS |
     MOJO_HANDLE_RIGHT_MAP_READABLE | MOJO_HANDLE_RIGHT_MAP_WRITABLE |
     MOJO_HANDLE_RIGHT_MAP_EXECUTABLE;
-
-TEST(CoreTest, GetTimeTicksNow) {
-  const MojoTimeTicks start = MojoGetTimeTicksNow();
-  EXPECT_NE(static_cast<MojoTimeTicks>(0), start)
-      << "MojoGetTimeTicksNow should return nonzero value";
-}
 
 // The only handle that's guaranteed to be invalid is |MOJO_HANDLE_INVALID|.
 // Tests that everything that takes a handle properly recognizes it.
@@ -86,15 +75,6 @@ TEST(CoreTest, InvalidHandle) {
   EXPECT_EQ(
       MOJO_RESULT_INVALID_ARGUMENT,
       MojoWaitMany(&h, &sig, 1u, MOJO_DEADLINE_INDEFINITE, nullptr, nullptr));
-
-  // Message pipe:
-  EXPECT_EQ(MOJO_RESULT_INVALID_ARGUMENT,
-            MojoWriteMessage(MOJO_HANDLE_INVALID, buffer, 0u, nullptr, 0u,
-                             MOJO_WRITE_MESSAGE_FLAG_NONE));
-  buffer_size = static_cast<uint32_t>(sizeof(buffer));
-  EXPECT_EQ(MOJO_RESULT_INVALID_ARGUMENT,
-            MojoReadMessage(MOJO_HANDLE_INVALID, buffer, &buffer_size, nullptr,
-                            nullptr, MOJO_READ_MESSAGE_FLAG_NONE));
 
   // Data pipe:
   MojoDataPipeProducerOptions dpp_options = {
@@ -150,115 +130,6 @@ TEST(CoreTest, InvalidHandle) {
   // This isn't an "invalid handle" test, but we'll throw it in here anyway
   // (since it involves a look-up).
   EXPECT_EQ(MOJO_RESULT_INVALID_ARGUMENT, MojoUnmapBuffer(nullptr));
-}
-
-TEST(CoreTest, BasicMessagePipe) {
-  MojoHandle h0, h1;
-  MojoHandleSignals sig;
-  char buffer[10] = {0};
-  uint32_t buffer_size;
-
-  h0 = MOJO_HANDLE_INVALID;
-  h1 = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoCreateMessagePipe(nullptr, &h0, &h1));
-  EXPECT_NE(h0, MOJO_HANDLE_INVALID);
-  EXPECT_NE(h1, MOJO_HANDLE_INVALID);
-  EXPECT_NE(h0, h1);
-
-  // Both handles should have the correct rights.
-  MojoHandleRights rights = MOJO_HANDLE_RIGHT_NONE;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoGetRights(h0, &rights));
-  EXPECT_EQ(kDefaultMessagePipeHandleRights, rights);
-  rights = MOJO_HANDLE_RIGHT_NONE;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoGetRights(h1, &rights));
-  EXPECT_EQ(kDefaultMessagePipeHandleRights, rights);
-
-  // Shouldn't be able to duplicate either handle (just test "with reduced
-  // rights" on one, and without on the other).
-  MojoHandle handle_denied = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_PERMISSION_DENIED,
-            MojoDuplicateHandleWithReducedRights(
-                h0, MOJO_HANDLE_RIGHT_DUPLICATE, &handle_denied));
-  EXPECT_EQ(MOJO_HANDLE_INVALID, handle_denied);
-  handle_denied = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_PERMISSION_DENIED,
-            MojoDuplicateHandle(h1, &handle_denied));
-  EXPECT_EQ(MOJO_HANDLE_INVALID, handle_denied);
-
-  // Shouldn't be readable, we haven't written anything.
-  MojoHandleSignalsState state;
-  EXPECT_EQ(MOJO_RESULT_DEADLINE_EXCEEDED,
-            MojoWait(h0, MOJO_HANDLE_SIGNAL_READABLE, 0, &state));
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_WRITABLE, state.satisfied_signals);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE |
-                MOJO_HANDLE_SIGNAL_PEER_CLOSED,
-            state.satisfiable_signals);
-
-  // Should be writable.
-  EXPECT_EQ(MOJO_RESULT_OK,
-            MojoWait(h0, MOJO_HANDLE_SIGNAL_WRITABLE, 0, &state));
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_WRITABLE, state.satisfied_signals);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE |
-                MOJO_HANDLE_SIGNAL_PEER_CLOSED,
-            state.satisfiable_signals);
-
-  // Last parameter is optional.
-  EXPECT_EQ(MOJO_RESULT_OK,
-            MojoWait(h0, MOJO_HANDLE_SIGNAL_WRITABLE, 0, nullptr));
-
-  // Try to read.
-  buffer_size = static_cast<uint32_t>(sizeof(buffer));
-  EXPECT_EQ(MOJO_RESULT_SHOULD_WAIT,
-            MojoReadMessage(h0, buffer, &buffer_size, nullptr, nullptr,
-                            MOJO_READ_MESSAGE_FLAG_NONE));
-
-  // Write to |h1|.
-  static const char kHello[] = "hello";
-  buffer_size = static_cast<uint32_t>(sizeof(kHello));
-  EXPECT_EQ(MOJO_RESULT_OK, MojoWriteMessage(h1, kHello, buffer_size, nullptr,
-                                             0, MOJO_WRITE_MESSAGE_FLAG_NONE));
-
-  // |h0| should be readable.
-  uint32_t result_index = 1;
-  MojoHandleSignalsState states[1];
-  sig = MOJO_HANDLE_SIGNAL_READABLE;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoWaitMany(&h0, &sig, 1, MOJO_DEADLINE_INDEFINITE,
-                                         &result_index, states));
-  EXPECT_EQ(0u, result_index);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE,
-            states[0].satisfied_signals);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE |
-                MOJO_HANDLE_SIGNAL_PEER_CLOSED,
-            states[0].satisfiable_signals);
-
-  // Read from |h0|.
-  buffer_size = static_cast<uint32_t>(sizeof(buffer));
-  EXPECT_EQ(MOJO_RESULT_OK,
-            MojoReadMessage(h0, buffer, &buffer_size, nullptr, nullptr,
-                            MOJO_READ_MESSAGE_FLAG_NONE));
-  EXPECT_EQ(static_cast<uint32_t>(sizeof(kHello)), buffer_size);
-  EXPECT_STREQ(kHello, buffer);
-
-  // |h0| should no longer be readable.
-  EXPECT_EQ(MOJO_RESULT_DEADLINE_EXCEEDED,
-            MojoWait(h0, MOJO_HANDLE_SIGNAL_READABLE, 10, &state));
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_WRITABLE, state.satisfied_signals);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE |
-                MOJO_HANDLE_SIGNAL_PEER_CLOSED,
-            state.satisfiable_signals);
-
-  // Close |h0|.
-  EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h0));
-
-  // |h1| should no longer be readable or writable.
-  EXPECT_EQ(
-      MOJO_RESULT_FAILED_PRECONDITION,
-      MojoWait(h1, MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_WRITABLE,
-               1000, &state));
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_PEER_CLOSED, state.satisfied_signals);
-  EXPECT_EQ(MOJO_HANDLE_SIGNAL_PEER_CLOSED, state.satisfiable_signals);
-
-  EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h1));
 }
 
 // TODO(ncbray): enable this test once NaCl supports the corresponding APIs.
@@ -737,36 +608,6 @@ TEST(CoreTest, MAYBE_BasicSharedBuffer) {
   EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h2));
 }
 
-TEST(CoreTest, MessagePipeChecksTransferRight) {
-  MojoHandle h0 = MOJO_HANDLE_INVALID;
-  MojoHandle h1 = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoCreateMessagePipe(nullptr, &h0, &h1));
-
-  // Create a shared buffer (which is transferrable and duplicatable).
-  MojoHandle h_transferrable = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_OK,
-            MojoCreateSharedBuffer(nullptr, 100, &h_transferrable));
-
-  // Make a non-transferrable duplicate handle.
-  MojoHandle h_not_transferrable = MOJO_HANDLE_INVALID;
-  EXPECT_EQ(MOJO_RESULT_OK, MojoDuplicateHandleWithReducedRights(
-                                h_transferrable, MOJO_HANDLE_RIGHT_TRANSFER,
-                                &h_not_transferrable));
-
-  // |h_transferrable| can be transferred.
-  EXPECT_EQ(MOJO_RESULT_OK, MojoWriteMessage(h0, nullptr, 0u, &h_transferrable,
-                                             1u, MOJO_WRITE_MESSAGE_FLAG_NONE));
-
-  // |h_not_transferrable| can be transferred.
-  EXPECT_EQ(MOJO_RESULT_PERMISSION_DENIED,
-            MojoWriteMessage(h0, nullptr, 0u, &h_not_transferrable, 1u,
-                             MOJO_WRITE_MESSAGE_FLAG_NONE));
-
-  EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h0));
-  EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h1));
-  EXPECT_EQ(MOJO_RESULT_OK, MojoClose(h_not_transferrable));
-}
-
 // This checks that things actually work in C (not C++).
 TEST(CoreTest, MinimalCTest) {
   const char* failure = MinimalCTest();
@@ -781,4 +622,3 @@ TEST(CoreTest, MinimalCppTest) {
 // TODO(vtl): Add multi-threaded tests.
 
 }  // namespace
-}  // namespace mojo
